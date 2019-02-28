37e89af87314e494a54a8f96e406662cc5672150
Zack <zkwhited@gmail.com>
2019-02-28 10:57:52 -0700

    interface w/o validation

--- lib/httpact.ex
+++ lib/httpact.ex
@@ -2,4 +2,8 @@ defmodule HTTPact do
   @moduledoc """
   An HTTP Contract used to decouple REST Api client libraries from Adapters.
   """
+
+  def execute(%HTTPact.Request{http_client: client} = request) do
+    client.execute(request)
+  end
 end
