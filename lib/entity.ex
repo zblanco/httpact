90a4d69d32c5d55f361af1c531082a32192c22b8
Zack <zkwhited@gmail.com>
2021-02-28 13:44:07 -0700

    remove fallback to any

--- lib/entity.ex
+++ lib/entity.ex
@@ -2,7 +2,6 @@ defprotocol HTTPact.Entity do
   @moduledoc """
   Provides a contract for an API Wrapper to convert responses into a return type.
   """
-  @fallback_to_any true
   def from_response(response, source_command)
 end
 
